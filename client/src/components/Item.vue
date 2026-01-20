aa6e27a968c6ee8eb482cab86e658c4dee2d789c
Uriel Fisher <urielfisher2004@gmail.com>
2026-01-20 21:53:24 +0200

    start css color method

---
+++ client/src/components/Item.vue
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<template>
+  <div class="item-card">
+    <img src="" alt="">
+    <h2>title</h2>
+    <button>add to cart</button>
+  </div>
+</template>
+
+<style scoped>
+	.item-card {
+		height: 300px;
+		width: 230px;
+		background-color: hsl(var(--pink)var(--high));
+	}
+</style>
